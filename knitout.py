4fe8db6c9812f0b3731ed19a775d50d77cfd2e47
Vidya Narayanan <vidyan@cmu.edu>
2019-02-04 17:47:31 -0500

    garter and rib examples

--- knitout.py
+++ knitout.py
@@ -198,6 +198,8 @@ class Writer:
 
 def example():
     stockinette_rectangle()
+    garter_rectangle()
+    rib_rectangle()
 
 def stockinette_rectangle(width=10, height=20):
     writer = Writer('1 2 3 4')
@@ -223,4 +225,70 @@ def stockinette_rectangle(width=10, height=20):
 
     writer.write('stockinette-'+str(width)+'x'+str(height)+'.k')
 
-#TODO more examples: garter_rectangle(), rib_rectangle(), seed_rectangle()
+def garter_rectangle(width=10, height=20):
+    writer = Writer('1 2 3 4')
+    writer.addHeader('Machine', 'swg')
+    carrier = '1'
+    writer.inhook(carrier)
+    for i in range(width-1, 0, -2):
+        writer.tuck('-', ('f',i), carrier)
+    writer.releasehook(carrier)
+    for i in range(0, width, 2):
+        writer.tuck('+', ('f', i), carrier)
+    for j in range(0, height):
+        if j%2 == 0:
+            for i in range(width, 0,-1):
+                writer.knit('-', ('f', i-1), carrier)
+        else:
+            for i in range(0, width):
+                writer.xfer(('f',i), ('b',i))
+            for i in range(0, width):
+                writer.knit('+', ('b', i), carrier)
+            for i in range(0, width):
+                writer.xfer(('b',i), ('f',i))
+
+    writer.outhook(carrier)
+    for i in range(0, width):
+        writer.drop(('f', i))
+
+    writer.write('garter-'+str(width)+'x'+str(height)+'.k')
+
+def rib_rectangle(width=10, height=20):
+    writer = Writer('1 2 3 4')
+    writer.addHeader('Machine', 'swg')
+    carrier = '1'
+    writer.inhook(carrier)
+    for i in range(width-1, 0, -2):
+        bed = 'f'
+        if i%2:
+            bed = 'b'
+        writer.tuck('-', (bed,i), carrier)
+    writer.releasehook(carrier)
+    for i in range(0, width, 2):
+        bed = 'f'
+        if i%2:
+            bed = 'b'
+
+        writer.tuck('+', (bed, i), carrier)
+    for j in range(0, height):
+        if j%2 == 0:
+            for i in range(width, 0,-1):
+                bed = 'f'
+                if (i-1)%2:
+                    bed = 'b'
+                writer.knit('-', (bed, i-1), carrier)
+        else:
+            for i in range(0, width):
+                bed = 'f'
+                if i%2:
+                    bed = 'b'
+                writer.knit('+', (bed, i), carrier)
+
+    writer.outhook(carrier)
+    for i in range(0, width):
+        writer.drop(('f', i))
+        writer.drop(('b', i))
+
+    writer.write('rib-'+str(width)+'x'+str(height)+'.k')
+
+
